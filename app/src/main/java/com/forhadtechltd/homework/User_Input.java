d8b66205fbefb6b93e2fc0b6154c016b46c209c2
Forhadul Islam <forhadul75@gmail.com>
2024-01-20 16:56:45 +0600

    firstn

--- app/src/main/java/com/forhadtechltd/homework/User_Input.java
+++ app/src/main/java/com/forhadtechltd/homework/User_Input.java
@@ -3,12 +3,45 @@ package com.forhadtechltd.homework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class User_Input extends AppCompatActivity {
+
+    EditText ipname, ipphone, ipemail, ippassword, ipconfirmpassword;
+    Button signup;
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_input_activity);
+
+        ipname = findViewById(R.id.ipname);
+        ipphone = findViewById(R.id.ipphone);
+        ipemail = findViewById(R.id.ipemail);
+        ippassword = findViewById(R.id.ippassword);
+        ipconfirmpassword = findViewById(R.id.ipconfirmpassword);
+        signup = findViewById(R.id.signup);
+        login = findViewById(R.id.login);
+
+        /*==============================================*/
+
+
+
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+
     }
 }
